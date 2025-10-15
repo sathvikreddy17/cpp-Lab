12d515ed63cc392049451732defd80cfbddf1f68
sathvikreddy17 <guralavishendrasathvikreddy@gmail.com>
2025-10-15 11:01:24 +0530

    Create C++ programs to demonstrate different forms of inheritance.cpp

---
+++ C++ programs to demonstrate different forms of inheritance.cpp
@@ -0,0 +1,21 @@
+#include<iostream> 
+using namespace std; 
+class vehicle{
+public:
+vehicle(){
+cout<<"This is a vehicle"<<endl;
+}
+};
+class car:public vehicle
+{
+public:
+};
+car(){
+cout<<"This vehicle is a car"<<endl;
+}
+int main()
+{
+cout<<"Roll No:24B11AI136"<<endl; 
+car obj;
+return 0;
+}
